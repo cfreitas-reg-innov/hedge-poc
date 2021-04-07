67814e095b52d6725c267ba73495659ab9499427
Caique Freitas <caique.freitas@reg-innov.com>
2021-04-08 01:44:42 +0200

    Include new Fake Components to test the contracts

--- src/contracts/Put.sol
+++ src/contracts/Put.sol
@@ -4,15 +4,26 @@ pragma solidity 0.8.3;
 import "./DaiToken.sol";
 import "./LiquidityPool.sol";
 import "./Options.sol";
+import "./FakeComponents.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Put is Options{
 
+    using SafeMath for uint256;
+
     string public name = "Put Hedge Option Contract";
     DaiToken token;
-    LiquidityPool pool;
+    LiquidityPool public pool;
+    uint256 public maxSpread = 95;
+    FakeSwap public fakeSwap; //update to real swap
 
-    constructor(DaiToken _token) Options(Options.OptionType.Put) {
+    constructor(DaiToken _token, 
+                FakePriceProvider _fakePriceProvider, // update to real price provider
+                FakeSwap _fakeSwap) // update to real swap
+                Options(Options.OptionType.Put, _fakePriceProvider) {
+
         token = _token;
+        fakeSwap = _fakeSwap;
         pool = new LiquidityPool(token);
         approve();
     }
@@ -27,26 +38,36 @@ contract Put is Options{
     /**
      * @notice Sends premiums to the ERC liquidity pool contract
      */
-    function sendPremium(uint256 amount) public {
-        /*
-        uint currentPrice = uint(priceProvider.latestAnswer());
+    function sendPremium(uint256 amount) public returns (uint premium){
+        
+        uint currentPrice = uint(fakePriceProvider.latestAnswer());
         address[] memory path = new address[](2);
-        path[0] = uniswapRouter.WETH();
+        path[0] = fakeSwap.WETH(); //update to real swap
         path[1] = address(token);
-        uint[] memory amounts = uniswapRouter.swapExactETHForTokens {
+        uint[] memory amounts = fakeSwap.swapExactETHForTokens {
             value: amount
         }(
             amount.mul(currentPrice).mul(maxSpread).div(1e10),
             path,
             address(this),
-            now
+            block.timestamp
         );
         premium = amounts[amounts.length - 1];
-        */
-        pool.sendPremium(amount);
+        //pool.sendPremium(amount);
     }
 
-
+    /**
+     * @notice Sends profits in DAI from the ERC pool to a put option holder's address
+     * @param option A specific option contract
+     */
+    function payProfit(Option memory option) internal override returns (uint profit) {
+        //uint currentPrice = uint(priceProvider.latestAnswer());
+        //require(option.strike >= currentPrice, "Current price is too high");
+        //profit = option.strike.sub(currentPrice).mul(option.amount).div(PRICE_DECIMALS);
+        profit = uint256(10000000000000000000);
+        pool.send(option.holder, profit);
+        //unlockFunds(option);
+    }
 
 
 }
--- test/LiquidityPool.test.js
+++ test/LiquidityPool.test.js
@@ -1,4 +1,5 @@
 const { assert } = require('chai')
+const BN = web3.utils.BN;
 
 const DaiToken = artifacts.require('DaiToken')
 const LiquidityPool = artifacts.require('LiquidityPool')
@@ -6,10 +7,6 @@ const LiquidityPool = artifacts.require('LiquidityPool')
 require('chai')
     .use(require('chai-as-promised'))
     .should()
-
-function tokens(n) {
-    return web3.utils.toWei(n, 'ether')
-}
 
 contract('LiquidityPool', ([holder1, holder2, liquidityProvider1, liquidityProvider2]) => {
     let daiToken, liquidityPool
@@ -19,47 +16,47 @@ contract('LiquidityPool', ([holder1, holder2, liquidityProvider1, liquidityProvi
         liquidityPool = await LiquidityPool.new(daiToken.address)
 
         // transfer initial quantity of DAI to the Holders
-        await daiToken.transfer(holder1, tokens('1000'))
-        await daiToken.transfer(holder2, tokens('1000'))
+        await daiToken.transfer(holder1, new BN(1000))
+        await daiToken.transfer(holder2, new BN(1000))
 
         // transfer initial quantity of DAI to the Liquidity Providers
-        await daiToken.transfer(liquidityProvider1, tokens('1000'))
-        await daiToken.transfer(liquidityProvider2, tokens('1000'))
+        await daiToken.transfer(liquidityProvider1, new BN(1000))
+        await daiToken.transfer(liquidityProvider2, new BN(1000))
     })
 
     describe('LP provides to the liquidity pool', async () => {
         it('has 100 DAI as balance after the first transference', async () => {
-            await daiToken.approve(liquidityPool.address, tokens('100'), {from: liquidityProvider1})
-            await liquidityPool.provide(tokens('100'), {from: liquidityProvider1})
+            await daiToken.approve(liquidityPool.address, new BN(100), {from: liquidityProvider1})
+            await liquidityPool.provide(new BN(100), {from: liquidityProvider1})
 
             let balance = await liquidityPool.totalBalance()    
-            assert.equal(balance.toString(), tokens('100'))
+            assert.equal(balance.toString(), new BN(100))
 
         })
 
         it('LP has correct share of 100 in the pool', async () => {
             let shareOf = await liquidityPool.shareOf(liquidityProvider1)
-            assert.equal(shareOf.toString(), tokens('100'))
+            assert.equal(shareOf.toString(), new BN(100))
         })
 
         it('Total Balance has 150 DAI after the second transference', async () => {
-            await daiToken.approve(liquidityPool.address, tokens('50'), {from: liquidityProvider2})
-            await liquidityPool.provide(tokens('50'), {from: liquidityProvider2})
+            await daiToken.approve(liquidityPool.address, new BN(50), {from: liquidityProvider2})
+            await liquidityPool.provide(new BN(50), {from: liquidityProvider2})
 
             let balance = await liquidityPool.totalBalance()
-            assert.equal(balance.toString(), tokens('150'))
+            assert.equal(balance.toString(), new BN(150))
         })
     })
     
     describe('LP withdraws from the liquidity pool', async () => {
         it('has 50 DAI as balance and 50 as supply after the withdraw', async () => {
-            await liquidityPool.withdraw(tokens('100'), {from: liquidityProvider1})
+            await liquidityPool.withdraw(new BN(100), {from: liquidityProvider1})
 
             let balance = await liquidityPool.totalBalance()
-            assert.equal(balance.toString(), tokens('50'))
+            assert.equal(balance.toString(), new BN(50))
 
             let shareOf = await liquidityPool.shareOf(liquidityProvider1)   
-            assert.equal(shareOf.toString(), tokens('0'))
+            assert.equal(shareOf.toString(), new BN(0))
 
         })
     })
